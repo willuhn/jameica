dee1ac77f07594019cf00f220a7cdd0c21a91ac2
willuhn <willuhn>
2004-08-09 21:03:15 +0000

    *** empty log message ***

---
+++ src/de/willuhn/jameica/system/BackgroundTask.java
@@ -0,0 +1,28 @@
+/**********************************************************************
+ * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/system/BackgroundTask.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2004/08/09 21:03:15 $
+ * $Author: willuhn $
+ * $Locker:  $
+ * $State: Exp $
+ *
+ * Copyright (c) by willuhn.webdesign
+ * All rights reserved
+ *
+ **********************************************************************/
+package de.willuhn.jameica.system;
+
+/**
+ */
+public interface BackgroundTask extends Runnable
+{
+
+}
+
+
+/**********************************************************************
+ * $Log: BackgroundTask.java,v $
+ * Revision 1.1  2004/08/09 21:03:15  willuhn
+ * *** empty log message ***
+ *
+ **********************************************************************/
