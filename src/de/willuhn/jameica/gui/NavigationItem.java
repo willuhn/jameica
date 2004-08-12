608e8b9cbe3bebc65b2596a152b6bf4c6c7aebc1
willuhn <willuhn>
2004-08-12 19:20:15 +0000

    *** empty log message ***

--- src/de/willuhn/jameica/gui/NavigationItem.java
+++ src/de/willuhn/jameica/gui/NavigationItem.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/gui/NavigationItem.java,v $
- * $Revision: 1.1 $
- * $Date: 2004/08/11 23:37:21 $
+ * $Revision: 1.2 $
+ * $Date: 2004/08/12 19:20:15 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -11,6 +11,8 @@
  *
  **********************************************************************/
 package de.willuhn.jameica.gui;
+
+import java.rmi.RemoteException;
 
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Listener;
@@ -23,34 +25,41 @@ import de.willuhn.datasource.GenericObjectNode;
 public interface NavigationItem extends GenericObjectNode
 {
 
-	/**
+  /**
 	 * Liefert das Bild, welches angezeigt wird, wenn das Element geoeffnet wird.
    * @return das Bild.
+   * @throws RemoteException
    */
-  public Image getIconOpen();
+  public Image getIconOpen() throws RemoteException;
   
 	/**
 	 * Liefert das Bild, welches angezeigt wird, wenn das Element geschlossen wird.
 	 * @return das Bild.
+   * @throws RemoteException
 	 */
-	public Image getIconClose();
+	public Image getIconClose() throws RemoteException;
 
 	/**
 	 * Anzuzeigender Name.
    * @return Name.
+   * @throws RemoteException
    */
-  public String getName();
+  public String getName() throws RemoteException;
   
   /**
    * Liefert den Listener, der ausgeloest werden soll, wenn das Element aktiviert wird.
    * @return Listener.
+   * @throws RemoteException
    */
-  public Listener getListener();
+  public Listener getListener() throws RemoteException;
 }
 
 
 /**********************************************************************
  * $Log: NavigationItem.java,v $
+ * Revision 1.2  2004/08/12 19:20:15  willuhn
+ * *** empty log message ***
+ *
  * Revision 1.1  2004/08/11 23:37:21  willuhn
  * @N Navigation ist jetzt modular erweiterbar
  *
