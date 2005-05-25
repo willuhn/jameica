3ab9a1ce86efc0f01a131b04fef2d9778301afc0
web0 <web0>
2005-05-25 16:11:47 +0000

    @N first code for extension system

---
+++ src/de/willuhn/jameica/gui/extension/ExtensionRegistry.java
@@ -0,0 +1,32 @@
+/**********************************************************************
+ * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/gui/extension/ExtensionRegistry.java,v $
+ * $Revision: 1.1 $
+ * $Date: 2005/05/25 16:11:47 $
+ * $Author: web0 $
+ * $Locker:  $
+ * $State: Exp $
+ *
+ * Copyright (c) by  bbv AG
+ * All rights reserved
+ *
+ **********************************************************************/
+
+package de.willuhn.jameica.gui.extension;
+
+/**
+ * 
+ */
+public class ExtensionRegistry
+{
+  public static void register(Extendable extendable)
+  {
+  }
+}
+
+
+/*********************************************************************
+ * $Log: ExtensionRegistry.java,v $
+ * Revision 1.1  2005/05/25 16:11:47  web0
+ * @N first code for extension system
+ *
+ *********************************************************************/
