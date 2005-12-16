4e2c513e8b1a5cb3e1b77930892f711b14be3bb5
web0 <web0>
2005-12-16 16:34:58 +0000

    @N new Constructor in TabGroup

--- src/de/willuhn/jameica/gui/util/TabGroup.java
+++ src/de/willuhn/jameica/gui/util/TabGroup.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/gui/util/TabGroup.java,v $
- * $Revision: 1.1 $
- * $Date: 2005/06/10 22:13:09 $
+ * $Revision: 1.2 $
+ * $Date: 2005/12/16 16:34:58 $
  * $Author: web0 $
  * $Locker:  $
  * $State: Exp $
@@ -33,24 +33,37 @@ public class TabGroup extends Container
 
   /**
    * ct.
-   * @param parent
-   * @param name
+   * Erzeugt eine neue Labelgroup.
+   * @param parent Das Composite, in dem die Group gemalt werden soll.
+   * @param name Name der Group.
    */
   public TabGroup(TabFolder parent, String name)
   {
-  	this(parent,name,false);
+  	this(parent,name,false,2);
   }
   
-	/**
-	 * ct.
-	 * Erzeugt eine neue Labelgroup, jedoch kann fastgelegt werden, ob
-	 * sie sich ueber die volle Hoehe der View erstreckt oder nur
-	 * auf ihre tatsaechliche Hoehe.
+  /**
+   * ct.
+   * Erzeugt eine neue Labelgroup, jedoch kann festgelegt werden, ob
+   * sie sich ueber die volle Hoehe der View erstreckt oder nur
+   * auf ihre tatsaechliche Hoehe.
    * @param parent Das Composite, in dem die Group gemalt werden soll.
    * @param name Name der Group.
    * @param fullSize true, wenn es voelle Hoehe haben soll.
    */
   public TabGroup(TabFolder parent, String name, boolean fullSize)
+  {
+    this(parent,name,false,2);
+  }
+
+  /**
+	 * ct.
+   * @param parent Das Composite, in dem die Group gemalt werden soll.
+   * @param name Name der Group.
+   * @param fullSize true, wenn es voelle Hoehe haben soll.
+   * @param cols Anzahl der Spalten. Per Default: 2.
+   */
+  public TabGroup(TabFolder parent, String name, boolean fullSize, int cols)
 	{
     super(fullSize);
 
@@ -61,7 +74,7 @@ public class TabGroup extends Container
     this.composite = new Composite(parent,SWT.NONE);
     this.composite.setBackground(Color.BACKGROUND.getSWTColor());
     this.composite.setFont(Font.H2.getSWTFont());
-    GridLayout layout = new GridLayout(2, false);
+    GridLayout layout = new GridLayout(cols, false);
     this.composite.setLayout(layout);
     GridData grid = new GridData(fullSize ? GridData.FILL_BOTH : GridData.FILL_HORIZONTAL);
     this.composite.setLayoutData(grid);
@@ -80,6 +93,9 @@ public class TabGroup extends Container
 
 /*********************************************************************
  * $Log: TabGroup.java,v $
+ * Revision 1.2  2005/12/16 16:34:58  web0
+ * @N new Constructor in TabGroup
+ *
  * Revision 1.1  2005/06/10 22:13:09  web0
  * @N new TabGroup
  * @N extended Settings
