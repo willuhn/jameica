07d283f98523009ddc76bbfbe6744257309b9fe2
willuhn <willuhn>
2006-07-13 08:18:40 +0000

    @B bug 250

--- build/jameica.bat
+++ build/jameica.bat
@@ -3,4 +3,4 @@
 REM Windows Start-Script fuer regulaeren Standalone-Betrieb.
 REM Jameica wird hierbei mit GUI gestartet.
 
-javaw.exe -Djava.library.path=lib/swt/win32 -jar jameica-win32.jar %1 %2 %3 %4 %5 %6 %7
+start javaw.exe -Djava.library.path=lib/swt/win32 -jar jameica-win32.jar %1 %2 %3 %4 %5 %6 %7
