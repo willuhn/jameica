013714c16e0b5da1bde6ab0e8edbcb64ee7938e0
willuhn <willuhn>
2006-11-09 22:45:44 +0000

    @N TERM is set to "dump" by default

--- build/jameica-macos.sh
+++ build/jameica-macos.sh
@@ -3,6 +3,8 @@
 # MacOS Start-Script fuer regulaeren Standalone-Betrieb.
 # Jameica wird hierbei mit GUI gestartet.
 
+TERM=xterm
+
 BASEDIR=`dirname $0`
 cd "$BASEDIR"
 exec java -XstartOnFirstThread -Djava.library.path="$BASEDIR/lib/swt/macos" -jar $BASEDIR/jameica-macos.jar $@
