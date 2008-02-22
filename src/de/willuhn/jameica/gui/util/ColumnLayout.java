a50b08d987b609c988eb23dbef7cb4e72ddebc47
willuhn <willuhn>
2008-02-22 16:40:16 +0000

    @N ColumnLayout erkennt selbst, ob das Parent bereits mehrspaltig ist und erstreckt sich ueber die volle Breite

--- src/de/willuhn/jameica/gui/util/ColumnLayout.java
+++ src/de/willuhn/jameica/gui/util/ColumnLayout.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/gui/util/ColumnLayout.java,v $
- * $Revision: 1.1 $
- * $Date: 2008/02/22 16:20:40 $
+ * $Revision: 1.2 $
+ * $Date: 2008/02/22 16:40:16 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -19,6 +19,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Layout;
 
 import de.willuhn.jameica.gui.Part;
 
@@ -52,7 +53,15 @@ public class ColumnLayout
     this.comp = new Composite(parent, SWT.NONE);
     this.comp.setBackground(Color.BACKGROUND.getSWTColor());
     
+    Layout parentLayout = parent.getLayout();
+    int colspan = 1;
+    if (parentLayout instanceof GridLayout)
+    {
+      colspan = ((GridLayout)parentLayout).numColumns;
+    }
     GridData grid = new GridData(fullSize ? GridData.FILL_BOTH : GridData.FILL_HORIZONTAL);
+    grid.horizontalSpan = colspan;
+
     this.comp.setLayoutData(grid);
 
     GridLayout layout = new GridLayout(columns < 1 ? 1 : columns, false);
@@ -87,6 +96,9 @@ public class ColumnLayout
 
 /*********************************************************************
  * $Log: ColumnLayout.java,v $
+ * Revision 1.2  2008/02/22 16:40:16  willuhn
+ * @N ColumnLayout erkennt selbst, ob das Parent bereits mehrspaltig ist und erstreckt sich ueber die volle Breite
+ *
  * Revision 1.1  2008/02/22 16:20:40  willuhn
  * @N Mehrspalten-Layouts
  *
