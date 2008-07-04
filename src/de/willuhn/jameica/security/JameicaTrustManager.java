28797d62e8234581691c9ae1a7571ea18f0b2ef9
willuhn <willuhn>
2008-07-04 17:43:21 +0000

    @C TrustManager generisch (nicht mehr explizit "SunX509") laden - dann geht auch OpenJDK

--- src/de/willuhn/jameica/security/JameicaTrustManager.java
+++ src/de/willuhn/jameica/security/JameicaTrustManager.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/security/JameicaTrustManager.java,v $
- * $Revision: 1.14 $
- * $Date: 2008/01/03 13:10:36 $
+ * $Revision: 1.15 $
+ * $Date: 2008/07/04 17:43:21 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -50,16 +50,9 @@ public class JameicaTrustManager implements X509TrustManager
     // einstuft, greifen wir ein und checken, ob wir das Zertifikat
     // in unserem eigenen Keystore haben.
 
-    String name = "SunX509";
-    String vendor = System.getProperty("java.vendor");
-    if (vendor != null && vendor.toLowerCase().indexOf("ibm") != -1)
-    {
-      Logger.info("seems to be an ibm java");
-      name = "IbmX509";
-    }
-
-    Logger.info("using trustmanager " + name);
-    TrustManagerFactory factory = TrustManagerFactory.getInstance(name);
+    Logger.info("init trustmanager manager [using algorithm: " + TrustManagerFactory.getDefaultAlgorithm() + "]");
+    TrustManagerFactory factory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+    Logger.info("using provider: " + factory.getProvider().getName());
     factory.init((KeyStore) null); // Wir initialisieren mit <code>null</code>, damit der System-Keystore genommen wird
 
     TrustManager[] trustmanagers = factory.getTrustManagers();
@@ -68,7 +61,7 @@ public class JameicaTrustManager implements X509TrustManager
       Logger.warn("NO system trustmanager found, will use only jameicas trustmanager");
       return;
     }
-
+    
     // uns interessiert nur der erste. Das ist der von Java selbst.
     this.standardTrustManager = (X509TrustManager) trustmanagers[0];
   }
@@ -257,6 +250,9 @@ public class JameicaTrustManager implements X509TrustManager
 
 /**********************************************************************
  * $Log: JameicaTrustManager.java,v $
+ * Revision 1.15  2008/07/04 17:43:21  willuhn
+ * @C TrustManager generisch (nicht mehr explizit "SunX509") laden - dann geht auch OpenJDK
+ *
  * Revision 1.14  2008/01/03 13:10:36  willuhn
  * @N mehr Debug-Ausgaben
  *
