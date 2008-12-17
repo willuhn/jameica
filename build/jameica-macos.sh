33395e93298fb857aa92772ec691b771421681d7
willuhn <willuhn>
2008-12-17 14:15:26 +0000

    @N Patch von Alexander

--- build/jameica-macos.sh
+++ build/jameica-macos.sh
@@ -5,6 +5,16 @@
 
 TERM=xterm
 
+if [ -x /System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Commands/java ]; then
+  JAVACMD="/System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Commands/java"
+elif [ -x /System/Library/Frameworks/JavaVM.framework/Versions/1.4.2/Commands/java ]; then
+  JAVACMD="/System/Library/Frameworks/JavaVM.framework/Versions/1.4.2/Commands/java"
+else
+  echo Fehler: Es wird Java 1.4.2 oder 1.5 benoetigt.
+  exit 1
+fi
+
 BASEDIR=`dirname "$0"`
 cd "$BASEDIR"
-exec java -Xmx128m -XstartOnFirstThread -Djava.library.path="$BASEDIR/lib/swt/macos" -jar "$BASEDIR/jameica-macos.jar" -o $@
+
+exec ${JAVACMD} -Xdock:name="Jameica" -Xmx128m -XstartOnFirstThread -Djava.library.path="$BASEDIR/lib/swt/macos" -jar "$BASEDIR/jameica-macos.jar" -o $@  >/dev/null
