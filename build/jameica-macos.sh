12259ab651bda5b3af5b164a780d4d0988cc4c01
willuhn <willuhn>
2010-02-09 21:58:53 +0000

    @N Aktualisiertes Start-Script fuer OSX, welches auch Java 1.6 prueft - siehe https://www.willuhn.de/bugzilla/show_bug.cgi?id=751

--- build/jameica-macos.sh
+++ build/jameica-macos.sh
@@ -3,18 +3,20 @@
 # MacOS Start-Script fuer regulaeren Standalone-Betrieb.
 # Jameica wird hierbei mit GUI gestartet.
 
-TERM=xterm
+TERM="xterm"
+JAVACMD=""
 
-if [ -x /System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Commands/java ]; then
-  JAVACMD="/System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Commands/java"
-elif [ -x /System/Library/Frameworks/JavaVM.framework/Versions/1.4.2/Commands/java ]; then
-  JAVACMD="/System/Library/Frameworks/JavaVM.framework/Versions/1.4.2/Commands/java"
-else
+for i in "1.4.2" "1.5" "1.6"; do
+  [ -x "/System/Library/Frameworks/JavaVM.framework/Versions/${i}/Commands/java" ] && \
+    JAVACMD="/System/Library/Frameworks/JavaVM.framework/Versions/${i}/Commands/java"
+done
+
+if [ -z "$JAVACMD" ]; then
   echo Fehler: Es wird Java 1.4.2 oder 1.5 benoetigt.
   exit 1
 fi
 
-BASEDIR=`dirname "$0"`
-cd "$BASEDIR"
+BASEDIR=$(dirname "$0")
+cd "${BASEDIR}"
 
-exec ${JAVACMD} -Xdock:name="Jameica" -Xmx256m -XstartOnFirstThread -jar "$BASEDIR/jameica-macos.jar" -o $@  >/dev/null
+exec ${JAVACMD} -d32 -Xdock:name="Jameica" -Xmx256m -XstartOnFirstThread -jar "${BASEDIR}/jameica-macos.jar" -o $@  >/dev/null
