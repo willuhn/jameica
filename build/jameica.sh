468c1c154eadf058712b751166e2f788a335f1ff
willuhn <willuhn>
2010-09-24 16:34:48 +0000

    @N Dennis's Patch zum Mergen der beiden Linux-Startscripts jameica.sh und jameica-linux64.sh in ein gemeinsames Script

--- build/jameica.sh
+++ build/jameica.sh
@@ -3,11 +3,14 @@
 # Linux Start-Script fuer regulaeren Standalone-Betrieb.
 # Jameica wird hierbei mit GUI gestartet.
 
-cd `dirname $(readlink -f $0)`
-
 #_JCONSOLE="-Dcom.sun.management.jmxremote"
 
 # https://www.willuhn.de/bugzilla/show_bug.cgi?id=774
 # https://www.willuhn.de/bugzilla/show_bug.cgi?id=798
 
-GDK_NATIVE_WINDOWS=1 java -Djava.net.preferIPv4Stack=true -Xmx256m $_JCONSOLE -jar jameica-linux.jar $@
+cd `dirname $(readlink -f $0)`
+bit=`arch |grep 64`
+if [ $? = 0 ]
+ then  GDK_NATIVE_WINDOWS=1 java -Djava.net.preferIPv4Stack=true -Xmx256m $_JCONSOLE -jar jameica-linux64.jar $@
+ else  GDK_NATIVE_WINDOWS=1 java -Djava.net.preferIPv4Stack=true -Xmx256m $_JCONSOLE -jar jameica-linux.jar $@
+fi
