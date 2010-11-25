cd44c1d40fa0ae879f8ed7d00a0fe88f51e97cce
willuhn <willuhn>
2010-11-25 11:34:08 +0000

    @N Error-View ueberarbeitet. Das System-Protokoll wird jetzt nicht mehr in die Zwischenablage kopiert sondern als Datei gespeichert. Das kann man als Attachment speichern und ist erheblich besser lesbar (keine Zeilenumbrueche in den Log-Ausgaben). Ausserdem enthaelt es jetzt auch noch Versionsinformationen zu OS, Java, Jameica und Plugins.

--- src/de/willuhn/jameica/gui/internal/views/FatalErrorView.java
+++ src/de/willuhn/jameica/gui/internal/views/FatalErrorView.java
@@ -1,8 +1,8 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/gui/internal/views/FatalErrorView.java,v $
- * $Revision: 1.8 $
- * $Date: 2006/03/15 16:25:32 $
- * $Author: web0 $
+ * $Revision: 1.9 $
+ * $Date: 2010/11/25 11:34:08 $
+ * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
  *
@@ -13,21 +13,33 @@
 
 package de.willuhn.jameica.gui.internal.views;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.io.Writer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
-import org.eclipse.swt.dnd.Clipboard;
-import org.eclipse.swt.dnd.TextTransfer;
-import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Text;
 
 import de.willuhn.jameica.gui.AbstractView;
 import de.willuhn.jameica.gui.Action;
 import de.willuhn.jameica.gui.GUI;
-import de.willuhn.jameica.gui.internal.action.Back;
-import de.willuhn.jameica.gui.parts.TextPart;
-import de.willuhn.jameica.gui.util.ButtonArea;
-import de.willuhn.jameica.gui.util.LabelGroup;
+import de.willuhn.jameica.gui.input.TextAreaInput;
+import de.willuhn.jameica.gui.internal.buttons.Back;
+import de.willuhn.jameica.gui.parts.ButtonArea;
+import de.willuhn.jameica.gui.util.Color;
+import de.willuhn.jameica.gui.util.Container;
+import de.willuhn.jameica.gui.util.SimpleContainer;
 import de.willuhn.jameica.messaging.StatusBarMessage;
+import de.willuhn.jameica.plugin.Manifest;
 import de.willuhn.jameica.system.Application;
 import de.willuhn.logging.Logger;
 import de.willuhn.logging.Message;
@@ -39,70 +51,89 @@ import de.willuhn.util.I18N;
  */
 public class FatalErrorView extends AbstractView
 {
+  private final static DateFormat DATEFORMAT = new SimpleDateFormat("yyyy-MM-dd");
+  private final static I18N i18n             = Application.getI18n();
 
   /**
    * @see de.willuhn.jameica.gui.AbstractView#bind()
    */
   public void bind() throws Exception
   {
-  	final I18N i18n = Application.getI18n();
-
 		try {
 			GUI.getView().setTitle(i18n.tr("Fehler"));
 	    
 	    Throwable t = (Throwable) getCurrentObject();
 	
-	    LabelGroup group = new LabelGroup(getParent(),i18n.tr("Unerwarteter Fehler"));
-	    
-	    group.addText(i18n.tr("Es ist ein unerwarteter Fehler aufgetreten.\n\n" +	      "Wenn Sie sich aktiv an der Verbesserung dieser Software beteiligen möchten, " +	      "dann klicken Sie einfach auf \"in Zwischenablage kopieren\", öffnen Ihr " +	      "Mailprogramm, fügen den Fehlertext via \"Bearbeiten/Einfügen\" in eine neue " +	      "Mail ein und senden ihn zusammen mit ihrer Beschreibung an den Autor dieses Programms.\n\n" +	      "Vielen Dank."),true);
+	    Container group = new SimpleContainer(getParent());
+	    group.addHeadline(i18n.tr("Unerwarteter Fehler"));
+	    group.addText(i18n.tr("Es ist ein unerwarteter Fehler aufgetreten."),true,Color.ERROR);
+	    group.addText(i18n.tr("Wenn Sie sich aktiv an der Verbesserung dieser Software beteiligen möchten, " +                    	      "dann klicken Sie einfach auf \"Diagnose-Protokoll speichern\" und senden " +                    	      "Sie die Datei zusammen mit einer kurzen Beschreibung per Mail an den Autor des Plugins.\n\n" +                    	      "Vielen Dank."),true);
 	
-			String e1 = "";
-			String e2 = "";
-			if (t != null)
+	    final String s = toString(t);
+
+	    if (t != null)
 			{
-				LabelGroup stacktrace = new LabelGroup(getParent(),i18n.tr("Stacktrace"),true);
-				TextPart text = new TextPart();
-				text.setAutoscroll(false);
-				text.setWordWrap(false);
-		    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		    t.printStackTrace(new PrintStream(bos));
-		    e1 = bos.toString();
-        e1 = e1.replaceAll("\r\n","\n");
-        text.appendText(e1);
-		
-		    e2 = "";
-		    Throwable t2 = t.getCause();
-		    if (t2 != null)
-		    {
-		      text.appendText("caused by");
-		  
-		      bos = new ByteArrayOutputStream();
-		      t2.printStackTrace(new PrintStream(bos));
-		      e2 += bos.toString();
-		      text.appendText(e2);
-		    }
-		    stacktrace.addPart(text);
+			  Container stacktrace = new SimpleContainer(getParent(),true);
+			  stacktrace.addHeadline(i18n.tr("Stacktrace"));
+			  TextAreaInput text = new TextAreaInput(s);
+			  text.paint(stacktrace.getComposite());
+			  ((Text)text.getControl()).setEditable(false);
 		  }
-	    final String s = e1 + e2;
-	    ButtonArea buttons = new ButtonArea(getParent(),2);
-      buttons.addButton(i18n.tr("Zurück"),new Back());
-	    buttons.addButton(i18n.tr("in Zwischenablage kopieren"),new Action()
+
+	    ButtonArea buttons = new ButtonArea();
+      buttons.addButton(new Back(true));
+	    buttons.addButton(i18n.tr("Diagnose-Protokoll speichern"),new Action()
       {
         public void handleAction(Object context) throws ApplicationException
         {
-					final Clipboard cb = new Clipboard(GUI.getDisplay());
-					TextTransfer textTransfer = TextTransfer.getInstance();
-					Message[] logEntries = Logger.getLastLines();
-					StringBuffer sb = new StringBuffer();
-					for (int i=0;i<logEntries.length;++i)
-					{
-						sb.append(logEntries[i].toString() + "\n");
-					}
-					final String log = "\n" + i18n.tr("Auszug aus dem Systemprotokoll") + ":\n" + sb.toString();
-					cb.setContents(new Object[]{(s + log)}, new Transfer[]{textTransfer});
-          Application.getMessagingFactory().sendMessage(new StatusBarMessage(Application.getI18n().tr("Stacktrace in Zwischenablage kopiert."),StatusBarMessage.TYPE_SUCCESS));
+          FileDialog fd = new FileDialog(GUI.getShell(),SWT.SAVE);
+          fd.setFileName("diagnose-" + DATEFORMAT.format(new Date()) + ".log");
+          fd.setFilterExtensions(new String[]{"*.log"});
+          fd.setFilterPath(System.getProperty("user.home"));
+          fd.setOverwrite(true);
+          
+          String f = fd.open();
+          if (f == null || f.length() == 0)
+            return;
+          
+          Writer writer = null;
+          try
+          {
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(f)),"iso-8859-1"));
+
+            writer.write("*** " + i18n.tr("System-Informationen") + " ***\n\n");
+            writer.write(getSystemStats() + "\n\n");
+
+            // Der Stack-Trace:
+            writer.write("*** " + i18n.tr("Stacktrace") + " ***\n\n");
+            writer.write(s + "\n\n");
+
+            writer.write("*** " + i18n.tr("Systemprotokoll") + " ***\n\n");
+            for (Message m:Logger.getLastLines())
+              writer.write(m + "\n");
+            
+            writer.close();
+            writer = null;
+            
+            Application.getMessagingFactory().sendMessage(new StatusBarMessage(Application.getI18n().tr("Diagnose-Protokoll gespeichert"),StatusBarMessage.TYPE_SUCCESS));
+          }
+          catch (Exception e)
+          {
+            Logger.error("error while writing diagnostic file",e);
+            Application.getMessagingFactory().sendMessage(new StatusBarMessage(Application.getI18n().tr("Fehler beim Speichern: {0}",e.getMessage()),StatusBarMessage.TYPE_ERROR));
+          }
+          finally
+          {
+            if (writer != null)
+            {
+              try {
+                writer.close();
+              } catch (Exception e) {/* useless */}
+            }
+          }
         }
-      },null,true);
+      },null,false,"document-save.png");
+	    buttons.paint(getParent());
 		}
 		catch (Exception e)
 		{
@@ -120,114 +151,63 @@ public class FatalErrorView extends AbstractView
   }        
 
   /**
-   * @see de.willuhn.jameica.gui.AbstractView#unbind()
+   * Liefert den Stacktrace des Fehlers.
+   * @param t der Fehler.
+   * @return der Stacktrace.
    */
-  public void unbind() throws ApplicationException
+  private String toString(Throwable t)
   {
+    if (t == null)
+      return "";
+    
+    StringBuffer sb = new StringBuffer();
+
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    t.printStackTrace(new PrintStream(bos));
+    sb.append(bos.toString());
+    return sb.toString().replaceAll("\r\n","\n");
+  }
+  
+  /**
+   * Liefert einen String mit den Versionen und installierten Plugins.
+   * @return String mit Versions-Infos.
+   */
+  private String getSystemStats()
+  {
+    StringBuffer sb = new StringBuffer();
+    
+    sb.append("Jameica: " + Application.getManifest().getVersion() + " (build date: " + Application.getManifest().getBuildDate() + ")\n\n");
+    sb.append("Plugins:\n");
+    List<Manifest> l = Application.getPluginLoader().getInstalledManifests();
+    for (Manifest mf:l)
+    {
+      sb.append("  " + mf.getName() + " " + mf.getVersion() + "(build date: " + mf.getBuildDate() + ")\n");
+    }
+
+    sb.append("\n");
+
+    sb.append("os.arch          : " + System.getProperty("os.arch") + "\n");
+    sb.append("os.name          : " + System.getProperty("os.name") + "\n");
+    sb.append("os.version       : " + System.getProperty("os.version") + "\n");
+
+    sb.append("\n");
+
+    sb.append("java.version     : " + System.getProperty("java.version") + "\n");
+    sb.append("java.vendor      : " + System.getProperty("java.vendor") + "\n");
+    sb.append("java.runtime.name: " + System.getProperty("java.runtime.name") + "\n");
+    sb.append("java.vm.name     : " + System.getProperty("java.vm.name") + "\n");
+
+    sb.append("\n");
+
+    sb.append("file.encoding    : " + System.getProperty("file.encoding") + "\n");
+    return sb.toString();
   }
 
 }
 
 /***************************************************************************
  * $Log: FatalErrorView.java,v $
- * Revision 1.8  2006/03/15 16:25:32  web0
- * @N Statusbar refactoring
- *
- * Revision 1.7  2006/01/05 16:10:46  web0
- * @C error handling
- *
- * Revision 1.6  2004/12/31 19:33:50  willuhn
- * *** empty log message ***
- *
- * Revision 1.5  2004/11/12 18:23:59  willuhn
- * *** empty log message ***
- *
- * Revision 1.4  2004/11/05 20:00:43  willuhn
- * @D javadoc fixes
- *
- * Revision 1.3  2004/10/25 17:59:15  willuhn
- * @N aenderbare Tabellen
- *
- * Revision 1.2  2004/10/20 12:33:53  willuhn
- * @C MVC-Refactoring (new Controllers)
- *
- * Revision 1.1  2004/10/08 13:38:20  willuhn
- * *** empty log message ***
- *
- * Revision 1.14  2004/07/21 23:54:54  willuhn
- * @C massive Refactoring ;)
- *
- * Revision 1.13  2004/07/20 22:52:49  willuhn
- * @C Refactoring
- *
- * Revision 1.12  2004/07/09 00:12:47  willuhn
- * @C Redesign
- *
- * Revision 1.11  2004/06/30 20:58:39  willuhn
- * *** empty log message ***
- *
- * Revision 1.10  2004/04/12 19:15:58  willuhn
- * @C refactoring
- * @N forms
- *
- * Revision 1.9  2004/03/30 22:08:26  willuhn
- * *** empty log message ***
- *
- * Revision 1.8  2004/03/24 00:46:02  willuhn
- * @C refactoring
- *
- * Revision 1.7  2004/03/06 18:24:24  willuhn
- * @D javadoc
- *
- * Revision 1.6  2004/03/03 22:27:10  willuhn
- * @N help texts
- * @C refactoring
- *
- * Revision 1.5  2004/02/26 18:47:03  willuhn
- * *** empty log message ***
- *
- * Revision 1.4  2004/02/22 20:05:21  willuhn
- * @N new Logo panel
- *
- * Revision 1.3  2004/02/20 20:45:24  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2004/01/28 20:51:25  willuhn
- * @C gui.views.parts moved to gui.parts
- * @C gui.views.util moved to gui.util
- *
- * Revision 1.1  2004/01/23 00:29:03  willuhn
- * *** empty log message ***
- *
- * Revision 1.5  2004/01/08 20:50:32  willuhn
- * @N database stuff separated from jameica
- *
- * Revision 1.4  2003/12/12 01:28:05  willuhn
- * *** empty log message ***
- *
- * Revision 1.3  2003/12/11 21:00:54  willuhn
- * @C refactoring
- *
- * Revision 1.2  2003/12/10 00:47:12  willuhn
- * @N SearchDialog done
- * @N FatalErrorView
- *
- * Revision 1.1  2003/12/09 11:38:50  willuhn
- * @N error page
- *
- * Revision 1.5  2003/12/05 17:12:23  willuhn
- * @C SelectInput
- *
- * Revision 1.4  2003/11/24 23:01:58  willuhn
- * @N added settings
- *
- * Revision 1.3  2003/10/29 00:41:27  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2003/10/23 22:36:34  willuhn
- * @N added Menu
- *
- * Revision 1.1  2003/10/23 21:50:06  willuhn
- * initial checkin
+ * Revision 1.9  2010/11/25 11:34:08  willuhn
+ * @N Error-View ueberarbeitet. Das System-Protokoll wird jetzt nicht mehr in die Zwischenablage kopiert sondern als Datei gespeichert. Das kann man als Attachment speichern und ist erheblich besser lesbar (keine Zeilenumbrueche in den Log-Ausgaben). Ausserdem enthaelt es jetzt auch noch Versionsinformationen zu OS, Java, Jameica und Plugins.
  *
  ***************************************************************************/
