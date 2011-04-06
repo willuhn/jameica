ef18374b75b44570efc91cb4c7b336d62a8b95ba
willuhn <willuhn>
2011-04-06 07:42:59 +0000

    @B BUGZILLA 1012

--- build/jameica-macos64.sh
+++ build/jameica-macos64.sh
@@ -4,10 +4,19 @@
 # Jameica wird hierbei mit GUI gestartet.
 
 TERM="xterm"
+
 JAVACMD="/usr/libexec/java_home/../java"
 
-if [ -z "$JAVACMD" ]; then
-  echo Fehler: Java nicht installiert.
+if [ ! -x "$JAVACMD" ]; then
+  JAVACMD="/System/Library/Frameworks/JavaVM.framework/Versions/Current/Commands/java"
+fi
+
+if [ ! -x "$JAVACMD" ]; then
+  JAVACMD="`which java`"
+fi
+
+if [ ! -x "$JAVACMD" ]; then
+  echo Fehler: Java nicht gefunden.
   exit 1
 fi
 
