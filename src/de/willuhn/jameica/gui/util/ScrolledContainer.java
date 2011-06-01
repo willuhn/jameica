f51cc51058db0200790260197af51ef20bc586e2
willuhn <willuhn>
2011-06-01 17:25:38 +0000

    @B Das uebernimmt sonst nicht automatisch die Default-Farbe des Parent

--- src/de/willuhn/jameica/gui/util/ScrolledContainer.java
+++ src/de/willuhn/jameica/gui/util/ScrolledContainer.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/gui/util/ScrolledContainer.java,v $
- * $Revision: 1.4 $
- * $Date: 2011/05/03 10:13:11 $
+ * $Revision: 1.5 $
+ * $Date: 2011/06/01 17:25:38 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -38,17 +38,34 @@ public class ScrolledContainer extends Container
    */
   public ScrolledContainer(Composite parent)
   {
+    this(parent,2);
+  }
+
+  /**
+   * ct.
+   * Erzeugt einen neuen Container.
+   * @param parent Das Composite, in dem das Composite gemalt werden soll.
+   * @param cols Anzahl der Spalten.
+   */
+  public ScrolledContainer(Composite parent, int cols)
+  {
     super(true);
+
+    org.eclipse.swt.graphics.Color bg = parent.getBackground();
     
     // BUGZILLA 412
     this.scrolled = new ScrolledComposite(parent,SWT.V_SCROLL);
     this.scrolled.setLayoutData(new GridData(GridData.FILL_BOTH));
     this.scrolled.setLayout(new FillLayout());
     this.scrolled.setExpandHorizontal(true);
+    if (bg != null)
+      this.scrolled.setBackground(bg);
 
     this.comp = new Composite(this.scrolled, SWT.NONE);
-    this.comp.setLayout(new GridLayout(2, false));
+    this.comp.setLayout(new GridLayout(cols, false));
     this.comp.setLayoutData(new GridData(GridData.FILL_BOTH));
+    if (bg != null)
+      this.comp.setBackground(bg);
     
     this.scrolled.setContent(this.comp);
 
@@ -83,7 +100,10 @@ public class ScrolledContainer extends Container
 
 /*********************************************************************
  * $Log: ScrolledContainer.java,v $
- * Revision 1.4  2011/05/03 10:13:11  willuhn
+ * Revision 1.5  2011/06/01 17:25:38  willuhn
+ * @B Das uebernimmt sonst nicht automatisch die Default-Farbe des Parent
+ *
+ * Revision 1.4  2011-05-03 10:13:11  willuhn
  * @R Hintergrund-Farbe nicht mehr explizit setzen. Erzeugt auf Windows und insb. Mac teilweise unschoene Effekte. Besonders innerhalb von Label-Groups, die auf Windows/Mac andere Hintergrund-Farben verwenden als der Default-Hintergrund
  *
  * Revision 1.3  2008-04-02 10:12:18  willuhn
