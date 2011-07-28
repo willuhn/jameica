e7f3d3d843b465d25d2885c3b6b4fa46f827f4dc
willuhn <willuhn>
2011-07-28 09:15:29 +0000

    @C Locking geaendert

--- src/de/willuhn/jameica/services/LockService.java
+++ src/de/willuhn/jameica/services/LockService.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/services/LockService.java,v $
- * $Revision: 1.4 $
- * $Date: 2011/05/24 12:28:36 $
+ * $Revision: 1.5 $
+ * $Date: 2011/07/28 09:15:29 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -15,26 +15,33 @@ package de.willuhn.jameica.services;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
 
 import de.willuhn.boot.BootLoader;
 import de.willuhn.boot.Bootable;
 import de.willuhn.boot.SkipServiceException;
+import de.willuhn.io.IOUtil;
 import de.willuhn.jameica.system.Application;
+import de.willuhn.jameica.system.JameicaException;
 import de.willuhn.logging.Logger;
-
 
 /**
  * Erzeugt die Lock-Datei.
  */
 public class LockService implements Bootable
 {
+  private FileChannel channel = null;
+  private FileLock lock       = null;
 
   /**
    * @see de.willuhn.boot.Bootable#depends()
    */
   public Class[] depends()
   {
-    return new Class[]{SecurityManagerService.class};
+    return new Class[] {SecurityManagerService.class};
   }
 
   /**
@@ -46,25 +53,53 @@ public class LockService implements Bootable
     if (Application.getStartupParams().isIgnoreLockfile())
       return;
     
-    File lock = new File(Application.getConfig().getWorkDir(),"jameica.lock");
-    Logger.info("creating lockfile " + lock.getAbsolutePath());
-    try {
-      if (lock.exists())
-      {
-        Logger.error("existing lock file found: " + lock);
-        throw new IOException("Lockfile allready exists");
-      }
-      lock.createNewFile();
-      lock.deleteOnExit();
-    }
-    catch (IOException ioe)
+    File file = new File(Application.getConfig().getWorkDir(),"jameica.lock");
+    
+    try
     {
-      if (!Application.getCallback().lockExists(lock.getAbsolutePath()))
-        System.exit(1);
+      // Lockfile neu anlegen, wenn es noch nicht existiert
+      if (!file.exists())
+      {
+        Logger.info("creating lockfile " + file);
+        if (!file.createNewFile())
+          throw new IOException("unable to create lock file " + file + ", is " + file.getParent() + " readonly?");
+        
+      }
       else
       {
-        lock.deleteOnExit();
+        Logger.info("lockfile " + file + " exists, checking if locked");
       }
+
+      // Beim Beenden loeschen wir die Datei grundsaetzlich, auch wenn wir
+      // sie selbst nicht erstellt haben. Sollte das Lock des anderen
+      // Prozesses noch drauf haengen, koennen wir sie eh nicht loeschen
+      file.deleteOnExit();
+
+      // OK, wir haben die Datei. Jetzt locken wir sie
+      this.channel = new RandomAccessFile(file, "rw").getChannel();
+      try
+      {
+        // Wenn die Datei bereits innerhalb der VM gelockt ist, wirft die Funktion eine OverlappingFileLockException.
+        // Wenn die Datei ausserhalb der VM gelockt wurde, liefert die Funktion NULL
+        this.lock = this.channel.tryLock();
+        
+        // In dem Fall werfen wir die Exception selbst
+        if (this.lock == null)
+          throw new OverlappingFileLockException();
+        
+        Logger.info(file + " locked");
+      }
+      catch (OverlappingFileLockException e)
+      {
+        Logger.error(file + " is locked - asking user, what to do");
+        if (!Application.getCallback().lockExists(file.getAbsolutePath()))
+          System.exit(1); // OK, der User will beenden
+      }
+    }
+    catch (IOException e)
+    {
+      Logger.error("unable to create/lock " + file,e);
+      throw new JameicaException(e); // Hier koennen wir nichts mehr machen
     }
   }
 
@@ -73,14 +108,29 @@ public class LockService implements Bootable
    */
   public void shutdown()
   {
+    try
+    {
+      if (this.lock != null)
+        this.lock.release();
+    }
+    catch (Exception e)
+    {
+      Logger.error("unable to release lock", e); // mehr koennen wir hier auch nicht machen
+    }
+    finally
+    {
+      IOUtil.close(this.channel);
+    }
   }
 
 }
 
-
 /**********************************************************************
  * $Log: LockService.java,v $
- * Revision 1.4  2011/05/24 12:28:36  willuhn
+ * Revision 1.5  2011/07/28 09:15:29  willuhn
+ * @C Locking geaendert
+ *
+ * Revision 1.4  2011-05-24 12:28:36  willuhn
  * @N Neuer zusaetzlicher Hinweis im Log: wegen http://forum.ubuntuusers.de/topic/jamaica-laesst-sich-nicht-starten/
  *
  * Revision 1.3  2009/08/17 09:29:22  willuhn
