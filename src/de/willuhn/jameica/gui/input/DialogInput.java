00d40ca032345f33ff198ffeb0d1e9b61452d3fc
willuhn <willuhn>
2011-10-20 16:16:53 +0000

    @C Loglevel

--- src/de/willuhn/jameica/gui/input/DialogInput.java
+++ src/de/willuhn/jameica/gui/input/DialogInput.java
@@ -1,7 +1,7 @@
 /**********************************************************************
  * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/gui/input/DialogInput.java,v $
- * $Revision: 1.27 $
- * $Date: 2011/08/08 10:45:05 $
+ * $Revision: 1.28 $
+ * $Date: 2011/10/20 16:16:53 $
  * $Author: willuhn $
  * $Locker:  $
  * $State: Exp $
@@ -85,7 +85,7 @@ public class DialogInput extends ButtonInput
 				}
         catch (OperationCanceledException oce)
         {
-          Logger.info("operation cancelled");
+          Logger.debug("operation cancelled");
         }
 				catch (Exception e1)
 				{
@@ -235,7 +235,10 @@ public class DialogInput extends ButtonInput
 
 /*********************************************************************
  * $Log: DialogInput.java,v $
- * Revision 1.27  2011/08/08 10:45:05  willuhn
+ * Revision 1.28  2011/10/20 16:16:53  willuhn
+ * @C Loglevel
+ *
+ * Revision 1.27  2011-08-08 10:45:05  willuhn
  * @C AbstractInput#update() ist jetzt "protected" (war package-private)
  *
  * Revision 1.26  2011-05-03 16:46:08  willuhn
@@ -244,170 +247,4 @@ public class DialogInput extends ButtonInput
  *
  * Revision 1.25  2011-05-03 10:13:11  willuhn
  * @R Hintergrund-Farbe nicht mehr explizit setzen. Erzeugt auf Windows und insb. Mac teilweise unschoene Effekte. Besonders innerhalb von Label-Groups, die auf Windows/Mac andere Hintergrund-Farben verwenden als der Default-Hintergrund
- *
- * Revision 1.24  2009/10/22 17:26:48  willuhn
- * @N setDialog in DialogInput
- *
- * Revision 1.23  2008/05/30 12:00:24  willuhn
- * @B Eingabefeld wurde auch dann grau gezeichnet, wenn nur der Button dahinter inaktiv ist
- *
- * Revision 1.22  2008/05/19 22:35:06  willuhn
- * @N setMaxLength(int) in DialogInput
- *
- * Revision 1.21  2008/01/17 23:46:05  willuhn
- * @B Bug 541
- *
- * Revision 1.20  2007/04/26 14:38:11  willuhn
- * @B Manuell eingegebenes Datum bei anschliessendem Oeffnen des Kalender-Dialogs uebernehmen
- * @N hasChanged in DialogInput ueberschrieben
- *
- * Revision 1.19  2007/01/23 15:52:10  willuhn
- * @C update() check for recursion
- * @N mandatoryCheck configurable
- *
- * Revision 1.18  2007/01/05 10:36:49  willuhn
- * @C Farbhandling - Jetzt aber!
- *
- * Revision 1.17  2007/01/05 09:41:53  willuhn
- * @C change color in DialogInput too
- *
- * Revision 1.16  2007/01/05 09:31:40  willuhn
- * @C change color in DialogInput too
- *
- * Revision 1.15  2006/12/28 15:35:52  willuhn
- * @N Farbige Pflichtfelder
- *
- * Revision 1.14  2005/06/23 23:03:25  web0
- * *** empty log message ***
- *
- * Revision 1.13  2005/03/09 01:06:37  web0
- * @D javadoc fixes
- *
- * Revision 1.12  2004/11/12 18:23:59  willuhn
- * *** empty log message ***
- *
- * Revision 1.11  2004/10/08 00:19:19  willuhn
- * *** empty log message ***
- *
- * Revision 1.10  2004/07/27 23:41:30  willuhn
- * *** empty log message ***
- *
- * Revision 1.9  2004/07/20 21:47:44  willuhn
- * @N ContextMenu
- *
- * Revision 1.8  2004/07/09 00:12:47  willuhn
- * @C Redesign
- *
- * Revision 1.7  2004/06/30 20:58:40  willuhn
- * *** empty log message ***
- *
- * Revision 1.6  2004/06/02 21:15:15  willuhn
- * @B win32 fixes in flat style
- * @C made ButtonInput more abstract
- *
- * Revision 1.5  2004/05/23 15:30:52  willuhn
- * @N new color/font management
- * @N new styleFactory
- *
- * Revision 1.4  2004/05/11 23:32:18  willuhn
- * *** empty log message ***
- *
- * Revision 1.3  2004/04/29 19:15:11  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2004/04/27 00:04:44  willuhn
- * @D javadoc
- *
- * Revision 1.1  2004/04/21 22:28:56  willuhn
- * *** empty log message ***
- *
- * Revision 1.1  2004/04/12 19:15:58  willuhn
- * @C refactoring
- * @N forms
- *
- * Revision 1.11  2004/04/05 23:29:26  willuhn
- * *** empty log message ***
- *
- * Revision 1.10  2004/03/25 00:45:49  willuhn
- * *** empty log message ***
- *
- * Revision 1.9  2004/03/24 00:46:03  willuhn
- * @C refactoring
- *
- * Revision 1.8  2004/03/11 08:56:55  willuhn
- * @C some refactoring
- *
- * Revision 1.7  2004/03/06 18:24:23  willuhn
- * @D javadoc
- *
- * Revision 1.6  2004/02/24 22:46:53  willuhn
- * @N GUI refactoring
- *
- * Revision 1.5  2004/02/23 20:30:34  willuhn
- * @C refactoring in AbstractDialog
- *
- * Revision 1.4  2004/02/22 20:05:21  willuhn
- * @N new Logo panel
- *
- * Revision 1.3  2004/02/18 20:28:45  willuhn
- * @N jameica now stores window position and size
- *
- * Revision 1.2  2004/02/18 01:40:30  willuhn
- * @N new white style
- *
- * Revision 1.1  2004/01/28 20:51:24  willuhn
- * @C gui.views.parts moved to gui.parts
- * @C gui.views.util moved to gui.util
- *
- * Revision 1.9  2004/01/23 00:29:03  willuhn
- * *** empty log message ***
- *
- * Revision 1.8  2003/12/29 16:29:47  willuhn
- * @N javadoc
- *
- * Revision 1.7  2003/12/20 16:52:45  willuhn
- * *** empty log message ***
- *
- * Revision 1.6  2003/12/16 02:27:44  willuhn
- * *** empty log message ***
- *
- * Revision 1.5  2003/12/11 21:00:54  willuhn
- * @C refactoring
- *
- * Revision 1.4  2003/12/10 00:47:12  willuhn
- * @N SearchDialog done
- * @N FatalErrorView
- *
- * Revision 1.3  2003/12/08 16:19:06  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2003/12/08 15:41:09  willuhn
- * @N searchInput
- *
- * Revision 1.1  2003/12/05 18:43:01  willuhn
- * *** empty log message ***
- *
- * Revision 1.7  2003/12/01 21:22:58  willuhn
- * *** empty log message ***
- *
- * Revision 1.6  2003/12/01 20:28:58  willuhn
- * @B filter in DBIteratorImpl
- * @N InputFelder generalisiert
- *
- * Revision 1.5  2003/11/24 14:21:53  willuhn
- * *** empty log message ***
- *
- * Revision 1.4  2003/11/24 11:51:41  willuhn
- * *** empty log message ***
- *
- * Revision 1.3  2003/11/22 20:43:05  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2003/11/21 02:10:21  willuhn
- * @N prepared Statements in AbstractDBObject
- * @N a lot of new SWT parts
- *
- * Revision 1.1  2003/11/20 03:48:42  willuhn
- * @N first dialogues
- *
  **********************************************************************/
