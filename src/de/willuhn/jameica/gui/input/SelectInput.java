0236a688151cfb4f3020ab26b83ec9346faf1992
Olaf Willuhn <github@willuhn.de>
2013-04-15 23:25:48 +0200

    @N SelectInput.getList

--- src/de/willuhn/jameica/gui/input/SelectInput.java
+++ src/de/willuhn/jameica/gui/input/SelectInput.java
@@ -1,15 +1,10 @@
 /**********************************************************************
- * $Source: /cvsroot/jameica/jameica/src/de/willuhn/jameica/gui/input/SelectInput.java,v $
- * $Revision: 1.45 $
- * $Date: 2011/08/11 10:36:53 $
- * $Author: willuhn $
- * $Locker:  $
- * $State: Exp $
  *
- * Copyright (c) by willuhn.webdesign
+ * Copyright (c) by Olaf Willuhn
  * All rights reserved
  *
  **********************************************************************/
+
 package de.willuhn.jameica.gui.input;
 
 import java.rmi.RemoteException;
@@ -244,6 +239,15 @@ public class SelectInput extends AbstractInput
     this.list = list;
     this.applyList();
   }
+  
+  /**
+   * Liefert die komplette Liste der Fachobjekte in der Liste.
+   * @return die komplette Liste der Fachobjekte in der Liste.
+   */
+  public List getList()
+  {
+    return this.list;
+  }
 
   /**
    * Formatiert die Bean passend fuer die Anzeige in der Combo-Box.
@@ -380,237 +384,3 @@ public class SelectInput extends AbstractInput
   }
 }
 
-/*********************************************************************
- * $Log: SelectInput.java,v $
- * Revision 1.45  2011/08/11 10:36:53  willuhn
- * @N PluginInput - Selectbox zur Auswahl eines Plugins
- * @N SelectInput#setList, um den Inhalt der Liste zur Laufzeit zu aendern
- *
- * Revision 1.44  2011-08-09 16:04:50  willuhn
- * @B Offset bei Vorhandensein eines "Bitte wahlen..."-Textes wurde nicht in setValue() beachtet
- *
- * Revision 1.43  2011-08-08 10:45:05  willuhn
- * @C AbstractInput#update() ist jetzt "protected" (war package-private)
- *
- * Revision 1.42  2011-05-03 16:46:08  willuhn
- * @R Flatstyle entfernt - war eh nicht mehr zeitgemaess und rendere auf aktuellen OS sowieso haesslich
- * @C SelectInput verwendet jetzt Combo statt CCombo - das sieht auf den verschiedenen OS besser aus
- *
- * Revision 1.41  2009/10/08 22:45:10  willuhn
- * @N Button "Geprueft" in Umsatz-Details, um einen Umsatz auch dort als geprueft markieren zu koennen
- * @N Button "Filter zuruecksetzen" in Kontoauszug
- *
- * Revision 1.40  2009/05/08 14:22:36  willuhn
- * @C preselected in SelectInput nullable
- *
- * Revision 1.39  2009/05/07 13:36:03  willuhn
- * @C Unterstuetzung fuer Mehrfachaufruf von getControl
- *
- * Revision 1.38  2009/01/04 16:59:44  willuhn
- * @N Format-Funktion zum Uberschreiben der Anzeige von Elementen in SelectInput
- *
- * Revision 1.37  2008/02/07 09:57:31  willuhn
- * @N Bug 550
- *
- * Revision 1.36  2008/02/06 11:02:04  willuhn
- * @B Bug 549
- *
- * Revision 1.35  2008/02/04 14:06:44  willuhn
- * @N SelectInput#setEditable
- *
- * Revision 1.34  2007/04/10 23:42:56  willuhn
- * @N TablePart Redesign (removed dependencies from GenericIterator/GenericObject)
- *
- * Revision 1.33  2007/04/02 23:29:59  willuhn
- * @R removed debug output
- * @B explicit cast
- *
- * Revision 1.32  2007/04/02 23:23:17  willuhn
- * @C 3. Konstruktor noch generischer
- *
- * Revision 1.31  2007/04/02 23:01:43  willuhn
- * @N SelectInput auf BeanUtil umgestellt
- *
- * Revision 1.30  2007/03/19 12:15:15  willuhn
- * @N Patch von Heiner
- *
- * Revision 1.29  2007/01/23 15:52:10  willuhn
- * @C update() check for recursion
- * @N mandatoryCheck configurable
- *
- * Revision 1.28  2007/01/05 10:36:49  willuhn
- * @C Farbhandling - Jetzt aber!
- *
- * Revision 1.27  2006/11/12 23:29:19  willuhn
- * @B small Bug (thanks to Reinhold)
- *
- * Revision 1.26  2006/10/16 23:04:24  willuhn
- * @B Bug 298
- *
- * Revision 1.25  2006/10/02 16:25:17  willuhn
- * @N Heiners zusaetzlicher Konstruktor
- *
- * Revision 1.24  2006/08/03 22:43:48  willuhn
- * *** empty log message ***
- *
- * Revision 1.23  2006/07/13 22:33:52  willuhn
- * *** empty log message ***
- *
- * Revision 1.22  2006/06/19 10:54:24  willuhn
- * @N neue Methode setEnabled(boolean) in Input
- * @N neue de_willuhn_util lib
- *
- * Revision 1.21  2006/01/02 17:37:49  web0
- * @N moved Velocity to Jameica
- *
- * Revision 1.20  2005/08/25 21:18:24  web0
- * @C changes accoring to findbugs eclipse plugin
- *
- * Revision 1.19  2005/08/22 13:31:52  web0
- * *** empty log message ***
- *
- * Revision 1.18  2004/12/13 22:48:31  willuhn
- * *** empty log message ***
- *
- * Revision 1.17  2004/12/05 17:29:19  willuhn
- * *** empty log message ***
- *
- * Revision 1.16  2004/11/15 00:38:20  willuhn
- * *** empty log message ***
- *
- * Revision 1.15  2004/11/12 18:23:59  willuhn
- * *** empty log message ***
- *
- * Revision 1.14  2004/10/17 16:28:45  willuhn
- * @N Die ersten Dauerauftraege abgerufen ;)
- *
- * Revision 1.13  2004/08/30 15:03:28  willuhn
- * @N neuer Security-Manager
- *
- * Revision 1.12  2004/08/18 23:14:19  willuhn
- * @D Javadoc
- *
- * Revision 1.11  2004/07/23 15:51:20  willuhn
- * @C Rest des Refactorings
- *
- * Revision 1.10  2004/07/21 23:54:53  willuhn
- * @C massive Refactoring ;)
- *
- * Revision 1.9  2004/07/09 00:12:47  willuhn
- * @C Redesign
- *
- * Revision 1.8  2004/06/30 20:58:40  willuhn
- * *** empty log message ***
- *
- * Revision 1.7  2004/06/18 19:47:17  willuhn
- * *** empty log message ***
- *
- * Revision 1.6  2004/06/17 00:05:26  willuhn
- * *** empty log message ***
- *
- * Revision 1.5  2004/05/23 15:30:52  willuhn
- * @N new color/font management
- * @N new styleFactory
- *
- * Revision 1.4  2004/05/04 23:05:16  willuhn
- * *** empty log message ***
- *
- * Revision 1.3  2004/04/27 00:04:44  willuhn
- * @D javadoc
- *
- * Revision 1.2  2004/04/24 19:05:05  willuhn
- * *** empty log message ***
- *
- * Revision 1.1  2004/04/12 19:15:58  willuhn
- * @C refactoring
- * @N forms
- *
- * Revision 1.10  2004/04/05 23:29:26  willuhn
- * *** empty log message ***
- *
- * Revision 1.9  2004/03/24 00:46:03  willuhn
- * @C refactoring
- *
- * Revision 1.8  2004/03/11 08:56:55  willuhn
- * @C some refactoring
- *
- * Revision 1.7  2004/03/06 18:24:23  willuhn
- * @D javadoc
- *
- * Revision 1.6  2004/03/03 22:27:10  willuhn
- * @N help texts
- * @C refactoring
- *
- * Revision 1.5  2004/02/25 23:11:57  willuhn
- * *** empty log message ***
- *
- * Revision 1.4  2004/02/24 22:46:53  willuhn
- * @N GUI refactoring
- *
- * Revision 1.3  2004/02/18 17:14:40  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2004/02/12 00:49:20  willuhn
- * *** empty log message ***
- *
- * Revision 1.1  2004/01/28 20:51:24  willuhn
- * @C gui.views.parts moved to gui.parts
- * @C gui.views.util moved to gui.util
- *
- * Revision 1.18  2004/01/23 00:29:03  willuhn
- * *** empty log message ***
- *
- * Revision 1.17  2004/01/08 20:50:32  willuhn
- * @N database stuff separated from jameica
- *
- * Revision 1.16  2004/01/06 20:11:21  willuhn
- * *** empty log message ***
- *
- * Revision 1.15  2003/12/29 16:29:47  willuhn
- * @N javadoc
- *
- * Revision 1.14  2003/12/16 02:27:44  willuhn
- * *** empty log message ***
- *
- * Revision 1.13  2003/12/11 21:00:54  willuhn
- * @C refactoring
- *
- * Revision 1.12  2003/12/10 23:51:55  willuhn
- * *** empty log message ***
- *
- * Revision 1.11  2003/12/05 17:12:23  willuhn
- * @C SelectInput
- *
- * Revision 1.10  2003/12/01 21:22:58  willuhn
- * *** empty log message ***
- *
- * Revision 1.9  2003/12/01 20:28:58  willuhn
- * @B filter in DBIteratorImpl
- * @N InputFelder generalisiert
- *
- * Revision 1.8  2003/11/24 23:01:58  willuhn
- * @N added settings
- *
- * Revision 1.7  2003/11/24 17:27:50  willuhn
- * @N Context menu in table
- *
- * Revision 1.6  2003/11/24 14:21:53  willuhn
- * *** empty log message ***
- *
- * Revision 1.5  2003/11/24 11:51:41  willuhn
- * *** empty log message ***
- *
- * Revision 1.4  2003/11/23 19:26:27  willuhn
- * *** empty log message ***
- *
- * Revision 1.3  2003/11/22 20:43:05  willuhn
- * *** empty log message ***
- *
- * Revision 1.2  2003/11/21 02:10:21  willuhn
- * @N prepared Statements in AbstractDBObject
- * @N a lot of new SWT parts
- *
- * Revision 1.1  2003/11/20 03:48:42  willuhn
- * @N first dialogues
- *
- **********************************************************************/
