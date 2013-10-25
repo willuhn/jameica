3f9c473fbbb280580fd925c49acc67067f60dd69
Olaf Willuhn <github@willuhn.de>
2013-10-25 23:40:12 +0200

    @N Queueing-Support

--- src/de/willuhn/jameica/messaging/MessageBus.java
+++ src/de/willuhn/jameica/messaging/MessageBus.java
@@ -24,9 +24,7 @@ public class MessageBus
    */
   public static void send(String queue, Object data)
   {
-    String s = StringUtils.trimToNull(queue);
-    MessagingQueue mq = (s != null ? Application.getMessagingFactory().getMessagingQueue(s) : Application.getMessagingFactory());
-    mq.sendMessage(new QueryMessage(data));
+    getQueue(queue).sendMessage(new QueryMessage(data));
   }
   
   /**
@@ -36,10 +34,31 @@ public class MessageBus
    */
   public static void sendSync(String queue, Object data)
   {
-    String s = StringUtils.trimToNull(queue);
-    MessagingQueue mq = (s != null ? Application.getMessagingFactory().getMessagingQueue(s) : Application.getMessagingFactory());
-    mq.sendSyncMessage(new QueryMessage(data));
+    getQueue(queue).sendSyncMessage(new QueryMessage(data));
   }
+  
+  /**
+   * Queued eine QueryMessage mit den angegebenen Nutzdaten asynchron an die Queue.
+   * Die Message bleibt solange in der Queue, bis ein Consumer vorhanden ist.
+   * @param queue die Queue.
+   * @param data die Nutzdaten.
+   */
+  public static void queue(String queue, Object data)
+  {
+    getQueue(queue).queueMessage(new QueryMessage(data));
+  }
+  
+  /**
+   * Liefert die passende Queue.
+   * @param queue der Queue-Name.
+   * @return die Queue.
+   */
+  private static MessagingQueue getQueue(String queue)
+  {
+    String s = StringUtils.trimToNull(queue);
+    return (s != null ? Application.getMessagingFactory().getMessagingQueue(s) : Application.getMessagingFactory());
+  }
+
 }
 
 
