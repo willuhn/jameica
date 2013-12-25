6581c99f7069ab03160d67a78c8529865004a230
Olaf Willuhn <github@willuhn.de>
2013-12-25 23:04:57 +0100

    System-Property "sun.security.smartcardio.library" wieder entfernt. Siehe https://www.willuhn.de/blog/index.php?/archives/654-Hibiscus-Experimenteller-PCSC-Support!.html#c1958

--- build/jameica.sh
+++ build/jameica.sh
@@ -8,16 +8,12 @@
 # https://www.willuhn.de/bugzilla/show_bug.cgi?id=774
 # https://www.willuhn.de/bugzilla/show_bug.cgi?id=798
 
-# Ergänzung für PCSC
-_PCSC64="-Dsun.security.smartcardio.library=/usr/lib64/libpcsclite.so.1"
-_PCSC32="-Dsun.security.smartcardio.library=/usr/lib/libpcsclite.so.1"
-
 link=$(readlink -f "$0")
 dir=$(dirname "$link")
 cd "$dir" 
 
 bit=`uname -m |grep 64`
 if [ $? = 0 ]
- then LIBOVERLAY_SCROLLBAR=0 GDK_NATIVE_WINDOWS=1 java -Xmx256m $_JCONSOLE $_PCSC64 -jar jameica-linux64.jar $@
- else LIBOVERLAY_SCROLLBAR=0 GDK_NATIVE_WINDOWS=1 java -Xmx256m $_JCONSOLE $_PCSC32 -jar jameica-linux.jar $@
+ then LIBOVERLAY_SCROLLBAR=0 GDK_NATIVE_WINDOWS=1 java -Xmx256m $_JCONSOLE -jar jameica-linux64.jar $@
+ else LIBOVERLAY_SCROLLBAR=0 GDK_NATIVE_WINDOWS=1 java -Xmx256m $_JCONSOLE -jar jameica-linux.jar $@
 fi
