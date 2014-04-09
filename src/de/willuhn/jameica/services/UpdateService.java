15579808787b58519d27da5ddece75dacff52c82
Olaf Willuhn <github@willuhn.de>
2014-04-09 23:50:57 +0200

    Regelmässige Prüfung nach Updates per Default aktiv

--- src/de/willuhn/jameica/services/UpdateService.java
+++ src/de/willuhn/jameica/services/UpdateService.java
@@ -142,7 +142,7 @@ public class UpdateService implements Bootable
    */
   public boolean getUpdateCheck()
   {
-    return settings.getBoolean("update.check",false);
+    return settings.getBoolean("update.check",true);
   }
 
   /**
