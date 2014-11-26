0fd5a9d7fbd236d75b1d86c75d3f191d1bc08d68
Olaf Willuhn <github@willuhn.de>
2014-11-26 09:35:27 +0100

    Auch wenn MD5 nicht mehr verwendet wird, sollte die Funktion trotzdem noch aus Gruenden der Abwaertskompatibilitaet zu anderen Plugins existieren.

--- src/de/willuhn/jameica/security/Certificate.java
+++ src/de/willuhn/jameica/security/Certificate.java
@@ -47,6 +47,17 @@ public class Certificate
   }
 
   /**
+   * Liefert den MD5-Fingerabdruck des Zertifikats.
+   * @return der MD5-Fingerabdruck des Zertifikats.
+   * @throws CertificateEncodingException
+   * @throws NoSuchAlgorithmException
+   */
+  public String getMD5Fingerprint() throws CertificateEncodingException, NoSuchAlgorithmException
+  {
+    return getFingerprint(Checksum.MD5);
+  }
+
+  /**
    * Liefert den SHA1-Fingerabdruck des Zertifikats.
    * @return der SHA1-Fingerabdruck des Zertifikats.
    * @throws CertificateEncodingException
