534b7799b1998fa99bcddab3f08b1cfc2a957679
Olaf Willuhn <github@willuhn.de>
2016-10-02 15:06:32 +0200

    BUGZILLA 1766 - Bin-Ordner auch in "target/classes" unterstützen (Maven-Kompatibilität) - siehe auch Pull-Request #16 (https://github.com/willuhn/jameica/pull/16)

--- src/de/willuhn/jameica/services/ClassService.java
+++ src/de/willuhn/jameica/services/ClassService.java
@@ -20,6 +20,7 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.regex.Pattern;
 
 import de.willuhn.boot.BootLoader;
 import de.willuhn.boot.Bootable;
@@ -153,13 +154,16 @@ public class ClassService implements Bootable
     // Wir fuegen das Verzeichnis zum ClassLoader hinzu. (auch fuer die Ressourcen)
     mycl.add(dir);
     
-    File bin = new File(dir,"bin");
-    if (bin.exists())
+    // Kompilat-Ordner aus den Quellcode-Projekten, um die Plugins auch in der IDE im Debugger laufen lassen zu koennen
+    for (File bin:Arrays.asList(new File(dir,"bin"),new File(dir,"target/classes")))
     {
-      Logger.info(mycl.getName() + ": added dir " + bin);
-      mycl.add(bin); // Fuer den Start in Eclipse bzw. entpackte Classen
+      if (bin.exists())
+      {
+        Logger.info(mycl.getName() + ": added dir " + bin);
+        mycl.add(bin); // Fuer den Start in Eclipse bzw. entpackte Classen
+      }
+      Application.getCallback().getStartupMonitor().addPercentComplete(2);
     }
-    Application.getCallback().getStartupMonitor().addPercentComplete(2);
 
     // Und jetzt noch alle darin befindlichen Jars
     File[] jars = mycl.addJars(dir);
@@ -211,6 +215,10 @@ public class ClassService implements Bootable
     
     String path = dir.getCanonicalPath();
     path = path.replaceAll("\\\\","/"); // Windows-Backslash gegen Linux-Slash ersetzen
+
+    
+    final Pattern binDir1 = Pattern.compile(path + "/bin/.*\\.class");
+    final Pattern binDir2 = Pattern.compile(path + "/target/classes.*\\.class");
     
     // Wir iterieren ueber alle Dateien in dem Verzeichnis.
     for (int i=0;i<child.length;++i)
@@ -222,10 +230,11 @@ public class ClassService implements Bootable
       name = name.replaceAll("\\\\","/"); // Windows-Backslash gegen Linux-Slash ersetzen
       
       // Class-Files nur, wenn sie sich im bin-Verzeichnis befinden
-      if (name.matches(path + "/bin/.*\\.class"))
+      boolean binDir = binDir1.matcher(name).matches();
+      if (binDir || binDir2.matcher(name).matches())
       {
         // Jetzt muessen wir vorn noch den Verzeichnisnamen abschneiden
-        name = name.substring(path.length() + 5); // fuehrenden Pfad abschneiden ("/bin" beachten)
+        name = name.substring(path.length() + (binDir ? 5 : 16)); // fuehrenden Pfad abschneiden ("/bin" bzw. "/target/classes" beachten)
         name = name.substring(0, name.indexOf(".class")).replace('/', '.'); // .class weg Trenner ersetzen
         if (name.startsWith("."))
           name = name.substring(1); // ggf. fuehrenden Punkt abschneiden
