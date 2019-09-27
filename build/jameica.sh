8697b0b4ac6d9532ffe829e36b71fe2c481c39bb
Olaf Willuhn <github@willuhn.de>
2019-09-27 09:47:41 +0200

    Launch-Script-Anpassung fuer ARM64

--- build/jameica.sh
+++ build/jameica.sh
@@ -12,7 +12,12 @@ link=$(readlink -f "$0")
 dir=$(dirname "$link")
 cd "$dir" 
 
-if uname -m |grep -q 64; then
+_JLP=""
+
+if uname -m |grep -q aarch64; then
+	archsuffix="arm64"
+	_JLP=" -Djava.library.path=lib/swt/linux-arm64"
+elif uname -m |grep -q 64; then
 	archsuffix="64"
 else
 	archsuffix=""
@@ -20,4 +25,4 @@ fi
 
 # Zur Anpassung der Skalierung auf HiDPI-Displays kann der Parameter "-Dswt.autoScale" verwendet werden.
 # Siehe https://www.willuhn.de/wiki/doku.php?id=develop:highdpi
-LIBOVERLAY_SCROLLBAR=0 GDK_NATIVE_WINDOWS=1 SWT_GTK3=1 exec java -Djava.net.preferIPv4Stack=true -Xmx512m -Xss64m $_JCONSOLE -jar jameica-linux${archsuffix}.jar $@
+LIBOVERLAY_SCROLLBAR=0 GDK_NATIVE_WINDOWS=1 SWT_GTK3=1 exec java $_JLP -Djava.net.preferIPv4Stack=true -Xmx512m -Xss64m $_JCONSOLE -jar jameica-linux${archsuffix}.jar $@
