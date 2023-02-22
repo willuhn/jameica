602898b4f76166de6d0b6e514472a7b6a9598760
Olaf Willuhn <github@willuhn.de>
2023-02-22 10:23:15 +0100

    Dialog etwas breiter anzeigen

--- src/de/willuhn/jameica/gui/internal/dialogs/AttachmentManageDialog.java
+++ src/de/willuhn/jameica/gui/internal/dialogs/AttachmentManageDialog.java
@@ -10,14 +10,11 @@
 
 package de.willuhn.jameica.gui.internal.dialogs;
 
-import java.io.IOException;
-
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 
 import de.willuhn.jameica.attachment.Attachment;
-import de.willuhn.jameica.attachment.Context;
 import de.willuhn.jameica.gui.dialogs.AbstractDialog;
 import de.willuhn.jameica.gui.internal.action.AttachmentAdd;
 import de.willuhn.jameica.gui.internal.action.AttachmentDelete;
@@ -28,23 +25,15 @@ import de.willuhn.jameica.gui.parts.Button;
 import de.willuhn.jameica.gui.parts.ButtonArea;
 import de.willuhn.jameica.gui.parts.TablePart;
 import de.willuhn.jameica.gui.util.SimpleContainer;
-import de.willuhn.jameica.messaging.StatusBarMessage;
-import de.willuhn.jameica.services.AttachmentService;
 import de.willuhn.jameica.system.Application;
-import de.willuhn.jameica.system.Settings;
-import de.willuhn.logging.Logger;
 
 /**
  * Verwaltet die Attachments für einen Dialog.
  */
 public class AttachmentManageDialog extends AbstractDialog
 {
-  private final static Settings settings = new Settings(AttachmentManageDialog.class);
-  final int WINDOW_WIDTH = 500;
+  final int WINDOW_WIDTH = 720;
   final int WINDOW_HEIGHT = 400;
-  
-  private AttachmentService service = Application.getBootLoader().getBootable(AttachmentService.class);
-  private Context ctx = null;
   
   /**
    * @param position die Position.
@@ -53,19 +42,7 @@ public class AttachmentManageDialog extends AbstractDialog
   {
     super(position);
     this.setTitle(Application.getI18n().tr("Dateianhänge"));
-    
-    
     this.setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
-
-    try
-    {
-      this.ctx = service.getContext();
-    }
-    catch (IOException e)
-    {
-      Logger.error("unable to determine current attachment context",e);
-      Application.getMessagingFactory().sendMessage(new StatusBarMessage(Application.getI18n().tr("Die Dateianhänge konnten nicht ermittelt werden"),StatusBarMessage.TYPE_ERROR));
-    }
   }
   
   /**
@@ -75,6 +52,7 @@ public class AttachmentManageDialog extends AbstractDialog
    protected void paint(Composite parent) throws Exception
   {
     SimpleContainer container = new SimpleContainer(parent,true);
+    container.addText(Application.getI18n().tr("Klicken Sie doppelt auf einen Dateianhang, um diesen zu öffnen oder mit der rechten Maustaste, um das Kontextmenü zu öffnen."),true);
     
     final TablePart table = new AttachmentListPart();
     table.paint(container.getComposite());
