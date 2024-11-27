c7af142b5e3039a08eaf4b0937cf417abacafe15
Olaf Willuhn <github@willuhn.de>
2024-11-27 10:36:29 +0100

    NO_AT_BRIDGE=1 hinzugefuegt - siehe https://github.com/eclipse-platform/eclipse.platform.swt/issues/670#issuecomment-1561560684

--- build/jameica.sh
+++ build/jameica.sh
@@ -22,4 +22,5 @@ fi
 
 # Zur Anpassung der Skalierung auf HiDPI-Displays kann der Parameter "-Dswt.autoScale" verwendet werden.
 # Siehe https://www.willuhn.de/wiki/doku.php?id=develop:highdpi
-LIBOVERLAY_SCROLLBAR=0 GDK_NATIVE_WINDOWS=1 SWT_GTK3=1 exec java $_JLP -Djava.net.preferIPv4Stack=true -Xmx512m -Xss64m $_JCONSOLE -jar jameica-linux${archsuffix}.jar $@
+# Zu NO_AT_BRIDGE=1 siehe https://github.com/eclipse-platform/eclipse.platform.swt/issues/670#issuecomment-1561560684
+NO_AT_BRIDGE=1 LIBOVERLAY_SCROLLBAR=0 GDK_NATIVE_WINDOWS=1 SWT_GTK3=1 exec java $_JLP -Djava.net.preferIPv4Stack=true -Xmx512m -Xss64m $_JCONSOLE -jar jameica-linux${archsuffix}.jar $@
