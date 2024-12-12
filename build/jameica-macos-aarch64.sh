c72753ec327ebda0b6db3473cbcbada6e3058535
Olaf Willuhn <github@willuhn.de>
2024-12-12 08:13:02 +0100

    Pfad zur Jameica JAR-Datei in jameica-macos-aarch64.sh falsch

--- build/jameica-macos-aarch64.sh
+++ build/jameica-macos-aarch64.sh
@@ -8,4 +8,4 @@ cd "${BASEDIR}"
 ## Wir verwenden generell nur noch die embedded Java-Version unter MacOS
 JAVACMD="${BASEDIR}/jre-macosaarch64/Contents/Home/bin/java"
 
-exec "${JAVACMD}" -Dsun.security.smartcardio.library=/System/Library/Frameworks/PCSC.framework/Versions/Current/PCSC -Xdock:name="Jameica" -Xmx512m -Xss64m -XstartOnFirstThread -jar "${BASEDIR}/jameica-macos64.jar" -o "$@"  >/dev/null
+exec "${JAVACMD}" -Dsun.security.smartcardio.library=/System/Library/Frameworks/PCSC.framework/Versions/Current/PCSC -Xdock:name="Jameica" -Xmx512m -Xss64m -XstartOnFirstThread -jar "${BASEDIR}/jameica-macos-aarch64.jar" -o "$@"  >/dev/null
