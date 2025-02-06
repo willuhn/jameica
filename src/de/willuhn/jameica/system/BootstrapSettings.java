eaca308725504f67f9da82ac70d21d1534ddbd8e
Sunny <sunny@sny.sh>
2025-02-06 12:31:53 +0100

    $XDG_CONFIG_HOME für .jameica.properties respektieren

--- src/de/willuhn/jameica/system/BootstrapSettings.java
+++ src/de/willuhn/jameica/system/BootstrapSettings.java
@@ -189,7 +189,15 @@ public class BootstrapSettings
   private static synchronized File getFile()
   {
     if (file == null)
-      file = new File(System.getProperty("user.home"),".jameica.properties");
+    {
+      String xdg = System.getenv("XDG_CONFIG_HOME");
+
+      if (xdg != null)
+        file = new File(xdg,"jameica.properties");
+      else
+        file = new File(System.getProperty("user.home"),".jameica.properties");
+    }
+
     return file;
   }
 }
